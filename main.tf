fae20232f61c7b6217d07e35a05a0136a6badf31
John Dezelick <john+git@dezelick.com>
2023-08-21 13:08:34 -0400

    update sg

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_vpc" "default" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -28,14 +29,25 @@ resource "aws_instance" "blog" {
   }
 }
 
+
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.1.0"
+  name    = "blog_new"
 
-  vpc_id  = data.aws_vpc.default.id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id = data.aws_vpc.default.id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
+
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "name" {
+  name    = "blog"
+  dedescription = "allow http"
+
+  vpc_id = data.aws_vpc.default.id
 }
