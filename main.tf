1246cac54b2cf6950f1d62f42a817ded294b3f18
John Dezelick <john+git@dezelick.com>
2023-08-21 13:28:28 -0400

    new dev vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
@@ -34,20 +47,13 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  name    = "blog"
+  vpc_id = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
-}
-
-resource "aws_security_group" "name" {
-  name    = "blog"
-  description = "allow http"
-
-  vpc_id = data.aws_vpc.default.id
 }
